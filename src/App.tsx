7b6de75761c92297ed46e478bb7983b09348d89f
dav4os <davi4os99@gmail.com>
2025-07-03 22:47:52 +0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import ErrorBoundary from './components/ErrorBoundary';
 import HomePage from './components/HomePage';
 import BlogPage from './components/BlogPage';
+import BlogArticle from './components/BlogArticle';
 import AboutPage from './components/AboutPage';
 import CarRentalPage from './components/CarRentalPage';
 
 function App() {
   return (
-    <div className="min-h-screen">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/blog" element={<BlogPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/rental" element={<CarRentalPage />} />
-      </Routes>
-    </div>
+    <ErrorBoundary>
+      <div className="min-h-screen">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/blog/:slug" element={<BlogArticle />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/rental" element={<CarRentalPage />} />
+        </Routes>
+      </div>
+    </ErrorBoundary>
   );
 }
 
